4f78e958592f6db4742972868747ac4735e02703
kaniba keita <kaniba.keita93@gmail.com>
2025-12-09 11:46:40 +0100

    feat: add favicon and icons for improved site branding

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -2,19 +2,21 @@ import ThemeRegistry from '@/lib/ThemeRegistry'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Kay | Portfolio',
-  description: 'Full-Stack | Data Viz | Impact',
-  icons: {
-    icon: "/favicon.ico",
-  },
+    title: 'Kay | Portfolio',
+    description: 'Full-Stack | Data Viz | Impact',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="fr">
-      <body>
-        <ThemeRegistry>{children}</ThemeRegistry>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="fr">
+            <head>
+                <link rel="icon" href="/favicon.ico?v=1" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1" />
+            </head>
+            <body>
+                <ThemeRegistry>{children}</ThemeRegistry>
+            </body>
+        </html>
+    )
 }
